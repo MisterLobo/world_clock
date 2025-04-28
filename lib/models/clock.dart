5d00f5a7ecd6c02b5036b3a5ad83c1eebe0337c7
MisterLobo <algae1234567890@gmail.com>
2025-04-28 21:05:17 +0800

    added removeClock function

--- lib/models/clock.dart
+++ lib/models/clock.dart
@@ -15,11 +15,20 @@ class Clock {
 Future<void> insertClock(Clock clock) async {
   final db = await initDb();
   int id = await db.rawInsert('INSERT INTO clocks(location) VALUES(?)', [clock.location]);
+  await db.close();
   debugPrint('ID: $id');
 }
 
 Future<List<Map>> getClocks() async {
   final db = await initDb();
   List<Map> clocks = await db.rawQuery('SELECT * FROM clocks');
+  await db.close();
   return clocks;
+}
+
+Future<int> removeClock(String location) async {
+  final db = await initDb();
+  final count = await db.rawDelete('DELETE FROM clocks WHERE location = ?', [location]);
+  await db.close();
+  return count;
 }
