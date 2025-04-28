806963ad30595e9c8319d766b323312af1b8d627
MisterLobo <algae1234567890@gmail.com>
2025-04-29 06:02:36 +0800

    upgrade and migrate to new sdk tools version

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_timezone
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
