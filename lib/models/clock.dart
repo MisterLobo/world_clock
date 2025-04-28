08a92db582d11e77f81c225be58fc90780f500b1
MisterLobo <algae1234567890@gmail.com>
2025-04-28 18:58:57 +0800

    added helper functions for getting and adding clocks

--- lib/models/clock.dart
+++ lib/models/clock.dart
@@ -1,14 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:world_clock/db.dart';
 
 class Clock {
-  final int id;
-  final String name;
+  final String location;
 
-  const Clock({required this.id, required this.name});
+  const Clock({required this.location});
 
-  Map<String, Object?> toMap() => { 'id': id, 'name': name };
+  Map<String, Object?> toMap() => { 'location': location };
 
   @override
-  String toString() => 'Clock{id: $id, name: $name}';
+  String toString() => 'Clock{location: $location}';
+}
 
-  Future<void> insertClock(Clock clock) async {}
+Future<void> insertClock(Clock clock) async {
+  final db = await initDb();
+  int id = await db.rawInsert('INSERT INTO clocks(location) VALUES(?)', [clock.location]);
+  debugPrint('ID: $id');
+}
+
+Future<List<Map>> getClocks() async {
+  final db = await initDb();
+  List<Map> clocks = await db.rawQuery('SELECT * FROM clocks');
+  return clocks;
 }
